9494c3bf3b88e6c945aafc932c885f42dd254ed3
qidian99 <qidian990107@163.com>
2019-02-06 14:46:52 -0800

    finished prompt height

--- app/src/main/java/com/example/joshuakang/team4_personalbest/MainActivity.java
+++ app/src/main/java/com/example/joshuakang/team4_personalbest/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.joshuakang.team4_personalbest;
 
+import android.content.SharedPreferences;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HeightPrompter.HeightPrompterListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPreferences = getSharedPreferences("user_height", MODE_PRIVATE);
+        String magnitude = sharedPreferences.getString("magnitude", "");
+        String metric = sharedPreferences.getString("metric", "");
+
+        if(magnitude == ""){
+            showHeightPrompt();
+        } else {
+            TextView textHeight = findViewById(R.id.textHeight);
+            textHeight.setText("Hi, your height is " + magnitude + " " + metric);
+        }
+
+    }
+
+
+    private void showHeightPrompt() {
+        FragmentManager fm = getSupportFragmentManager();
+        HeightPrompter editNameDialogFragment = HeightPrompter.newInstance(getString(R.string.heightPrompt));
+        editNameDialogFragment.show(fm, "fragment_prompt_height");
+    }
+
+    @Override
+    public void onFinishEditDialog(String[] inputText) {
+        TextView textHeight = findViewById(R.id.textHeight);
+        SharedPreferences sharedPreferences = getSharedPreferences("user_height", MODE_PRIVATE);
+        SharedPreferences.Editor editor =   sharedPreferences.edit();
+        editor.putString("magnitude", inputText[0]);
+        editor.putString("metric", inputText[1]);
+        editor.apply();
+        textHeight.setText("Hi, your height is " + inputText[0] + " " + inputText[1]);
+        Toast.makeText(this, "Height saved", Toast.LENGTH_SHORT);
     }
 }
