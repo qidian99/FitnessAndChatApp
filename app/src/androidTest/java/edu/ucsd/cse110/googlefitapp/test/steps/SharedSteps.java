585b483a0b01a0143839fb52290a470e35a27847
qidian99 <qidian990107@163.com>
2019-02-15 00:33:48 -0800

    sucessfully added cucumber test (prep for MS2) #1

--- app/src/androidTest/java/edu/ucsd/cse110/googlefitapp/test/steps/SharedSteps.java
+++ app/src/androidTest/java/edu/ucsd/cse110/googlefitapp/test/steps/SharedSteps.java
@@ -1,5 +1,6 @@
 package edu.ucsd.cse110.googlefitapp.test.steps;
 
+import android.content.Intent;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.espresso.matcher.ViewMatchers;
@@ -19,6 +20,7 @@ import edu.ucsd.cse110.googlefitapp.R;
 import edu.ucsd.cse110.googlefitapp.StepCountActivity;
 import edu.ucsd.cse110.googlefitapp.fitness.FitnessService;
 import edu.ucsd.cse110.googlefitapp.fitness.FitnessServiceFactory;
+import gherkin.cli.Main;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -39,7 +41,8 @@ import static org.hamcrest.core.IsAnything.anything;
 import static org.hamcrest.core.StringContains.containsString;
 
 public class SharedSteps {
-    private static final String TEST_SERVICE = "TEST_SERVICE";
+    private static final String TEST_SERVICE_MAIN_ACTIVITY = "TEST_SERVICE_MAIN_ACTIVITY";
+    private static final String TEST_SERVICE_STEP_ACTIVITY = "TEST_SERVICE_STEP_ACTIVITY";
 
     private ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity
             .class);
@@ -51,14 +54,35 @@ public class SharedSteps {
 
     @Before
     public void setup() {
-        FitnessServiceFactory.put(TEST_SERVICE, new FitnessServiceFactory.BluePrint() {
+        FitnessServiceFactory.put(TEST_SERVICE_MAIN_ACTIVITY, new FitnessServiceFactory.BluePrint() {
             @Override
             public FitnessService create(StepCountActivity stepCountActivity) {
-                return new TestFitnessService(stepCountActivity);
+                return null;
+            }
+
+            @Override
+            public FitnessService create(MainActivity mainActivity) {
+                return new TestMainFitnessService(mainActivity);
             }
         });
-        mActivityTestRule.getActivity().setFitnessServiceKey(TEST_SERVICE);
-        mActivityTestRule.launchActivity(null);
+        FitnessServiceFactory.put(TEST_SERVICE_STEP_ACTIVITY, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(StepCountActivity stepCountActivity) {
+                return null;
+            }
+
+            @Override
+            public FitnessService create(MainActivity mainActivity) {
+                return null;
+            }
+        });
+
+        Intent intent = new Intent();
+        intent.putExtra("TEST", true);
+        intent.putExtra("TEST_SERVICE_MAIN", TEST_SERVICE_MAIN_ACTIVITY);
+        intent.putExtra("TEST_SERVICE_STEP_COUNT", TEST_SERVICE_STEP_ACTIVITY);
+        mActivityTestRule.launchActivity(intent);
+        mActivityTestRule.getActivity().setFitnessServiceKey(TEST_SERVICE_STEP_ACTIVITY);
 
         Intents.init();
 //        nameIdMap.put("first", "number_1");
@@ -88,55 +112,51 @@ public class SharedSteps {
 
     @Given("^Sarah has successfully downloaded the app$")
     public void sarahHasSuccessfullyDownloadedTheApp() throws Throwable {
-        // Should clear any shared preference and restart the app if necessary to make it
-        // act like newly downloaded app
         assertThat(mActivityTestRule.getActivity(), notNullValue());
     }
 
     @And("^she has accepted all the permissions$")
-    public void sheHasAcceptedAllThePermissions() throws Throwable {
-    }
+    public void sheHasAcceptedAllThePermissions() throws Throwable {}
 
     @And("^she uses feet and inches for her height$")
-    public void sheUsesFeetAndInchesForHerHeight() throws Throwable {
-    }
+    public void sheUsesFeetAndInchesForHerHeight() throws Throwable {}
 
     @When("^the application asks for her height, she$")
     public void theApplicationAsksForHerHeightShe() throws Throwable {
-//        onView(withId(R.id.clearBtn))
-//                .check(matches((withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))));
-//        onView(withId(R.id.clearBtn)).perform(click());
+        onView(withId(R.id.clearBtn))
+                .check(matches((withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))));
+        onView(withId(R.id.clearBtn)).perform(click());
     }
 
     @Then("^chooses the feet and inches option in the drop-down menu$")
     public void choosesTheFeetAndInchesOptionInTheDropDownMenu() throws Throwable {
-//        // Make sure a dialog show up
-//        onView(withText(R.string.heightPrompt))
-//        .inRoot(isDialog()) // <---
-//        .check(matches(isDisplayed()));
-//
-//        // Choose feet and inches
-//        onView(withId(R.id.metricSpinner)).perform(click());
-//        onData(anything()).inRoot(RootMatchers.isPlatformPopup()).atPosition(1).perform(click());
-//
-//        onView(withId(R.id.metricSpinner)).check(matches(withSpinnerText(containsString("ft"))));
-//        onView(withId(R.id.cent_height))
-//                .check(matches((withEffectiveVisibility(ViewMatchers.Visibility.GONE))));
-//        onView(withId(R.id.ft_height))
-//                .check(matches((withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))));
-//        onView(withId(R.id.inch_height))
-//                .check(matches((withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))));
+        // Make sure a dialog show up
+        onView(withText(R.string.heightPrompt))
+        .inRoot(isDialog()) // <---
+        .check(matches(isDisplayed()));
+
+        // Choose feet and inches
+        onView(withId(R.id.metricSpinner)).perform(click());
+        onData(anything()).inRoot(RootMatchers.isPlatformPopup()).atPosition(1).perform(click());
+
+        onView(withId(R.id.metricSpinner)).check(matches(withSpinnerText(containsString("ft"))));
+        onView(withId(R.id.cent_height))
+                .check(matches((withEffectiveVisibility(ViewMatchers.Visibility.GONE))));
+        onView(withId(R.id.ft_height))
+                .check(matches((withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))));
+        onView(withId(R.id.inch_height))
+                .check(matches((withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))));
     }
 
     @And("^inputs (\\d+) in the first textbox and (\\d+) in the second textbox$")
     public void inputsInTheFirstTextboxAndInTheSecondTextbox(int arg0, int arg1) throws Throwable {
-//        onView(withId(R.id.ft_height)).perform(typeText(String.valueOf(arg0)));
-//        onView(withId(R.id.inch_height)).perform(typeText(String.valueOf(arg1)));
+        onView(withId(R.id.ft_height)).perform(typeText(String.valueOf(arg0)));
+        onView(withId(R.id.inch_height)).perform(typeText(String.valueOf(arg1)));
     }
 
     @When("^she presses the “Done” button$")
     public void shePressesTheDoneButton() throws Throwable {
-//        onView(withId(R.id.posBtn)).perform(click());
+        onView(withId(R.id.posBtn)).perform(click());
     }
 
     @Then("^she is taken to the home screen.$")
@@ -146,11 +166,53 @@ public class SharedSteps {
 //                .inRoot(isDialog()) // <---
 //                .check(matches(not(isDisplayed())));
     }
-    private class TestFitnessService implements FitnessService {
-        private static final String TAG = "[TestFitnessService]: ";
+
+    private class TestMainFitnessService implements FitnessService {
+        private static final String TAG = "[TestMainFitnessService]: ";
+        private MainActivity mainActivity;
+
+        public TestMainFitnessService(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public int getRequestCode() {
+            return 0;
+        }
+
+        @Override
+        public void setup() {
+            System.out.println(TAG + "setup");
+        }
+
+        @Override
+        public void updateStepCount() {
+            System.out.println(TAG + "update all texts");
+            mainActivity.updateAll(1000);
+        }
+
+        @Override
+        public void stopAsync() {
+
+        }
+
+
+        @Override
+        public void startAsync() {
+
+        }
+
+        @Override
+        public boolean hasPermission() {
+            return true;
+        }
+    }
+
+    private class TestStepCountFitnessService implements FitnessService {
+        private static final String TAG = "[TestStepCountFitnessService]: ";
         private StepCountActivity stepCountActivity;
 
-        public TestFitnessService(StepCountActivity stepCountActivity) {
+        public TestStepCountFitnessService(StepCountActivity stepCountActivity) {
             this.stepCountActivity = stepCountActivity;
         }
 
@@ -167,7 +229,7 @@ public class SharedSteps {
         @Override
         public void updateStepCount() {
             System.out.println(TAG + "updateStepCount");
-            stepCountActivity.setStepCount(1337);
+            stepCountActivity.setStepCount(1000);
         }
 
         @Override
@@ -175,14 +237,15 @@ public class SharedSteps {
 
         }
 
-        @Override
-        public void updateActivity() {
-
-        }
 
         @Override
         public void startAsync() {
 
+        }
+
+        @Override
+        public boolean hasPermission() {
+            return true;
         }
     }
 
