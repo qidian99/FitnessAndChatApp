8f7efa585962025954bb17d8fa404b50ac2608e2
EnqiZhang99 <enz004@ucsd.edu>
2019-02-15 13:46:28 -0800

    resolved conflict #1

--- app/src/main/java/edu/ucsd/cse110/googlefitapp/Encouragement.java
+++ app/src/main/java/edu/ucsd/cse110/googlefitapp/Encouragement.java
@@ -1,31 +1,46 @@
 package edu.ucsd.cse110.googlefitapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.Toast;
 
 public class Encouragement {
 
-    private Activity activity;
     private static boolean turnOn;
+    private static boolean onlyDaily = false;
     private static final int QUARTER_MSG = 0;
     private static final int HALFWAY_MSG = 1;
     private static final int CLOSE_MSG = 2;
+    private Activity activity;
+
     private String[] msgs =
             {"You are on your way. Keep it up!",
             "You are halfway to your goal!",
-            "You are really close to your goal!",
+            "You are close to your goal!",
             "You have nearly doubled your steps since yesterday!"};
 
-    public Encouragement(Activity activity) {
+    /* ctor */
+    public Encouragement(Activity activity, boolean onlyDaily) {
         this.activity = activity;
+        this.onlyDaily = onlyDaily;
     }
 
-    public void turnOnEncouragement() {
+    public static void turnOnEncouragement() {
         turnOn = true;
     }
 
-    public void turnOffEncouragement() {
+    public static void turnOffEncouragement() {
         turnOn = false;
+    }
+
+    public static void setOnlyDaily() {
+        onlyDaily = true;
+
+    }
+
+    public static void setEveryLaunch() {
+        onlyDaily = false;
     }
 
     /*public boolean isEncouragementOn() {}*/
@@ -50,27 +65,29 @@ public class Encouragement {
         if (40 < percent && percent <= 60) {
             return msgs[HALFWAY_MSG];
         }
-        if (60 < percent && percent <= 60) {
+        if (80 < percent && percent <= 95) {
             return msgs[CLOSE_MSG];
         }
         return msgs[QUARTER_MSG];
     }
 
     private String determinePassiveMessage(long current, long goal) {
+
         double percent = getPercent(current,goal);
+        /*Toast.makeText(activity,new String(Double.toString(percent)),Toast.LENGTH_LONG).show();*/
         if (0 < percent && percent <= 40) {
             return msgs[QUARTER_MSG];
         }
         if (40 < percent && percent <= 60) {
             return msgs[HALFWAY_MSG];
         }
-        if (60 < percent && percent <= 60) {
+        if (80 < percent && percent <= 95) {
             return msgs[CLOSE_MSG];
         }
         return msgs[QUARTER_MSG];
     }
 
     private double getPercent(long current, long goal) {
-        return current/(double)goal;
+        return ((double)current/goal) * 100;
     }
 }
