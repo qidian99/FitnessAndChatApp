a0cdb3ce2749c9c61ab2c95fa10f623f83f027a1
EnqiZhang99 <enz004@ucsd.edu>
2019-03-03 23:43:44 -0800

    Finish Dialog Roboletric test #1

--- app/src/test/java/edu/ucsd/cse110/googlefitapp/NewGoalDialogUnitTest.java
+++ app/src/test/java/edu/ucsd/cse110/googlefitapp/NewGoalDialogUnitTest.java
@@ -1,10 +1,24 @@
 package edu.ucsd.cse110.googlefitapp;
 
+import android.content.Intent;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.widget.Button;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 
 import edu.ucsd.cse110.googlefitapp.dialog.NewGoalDialog;
+import edu.ucsd.cse110.googlefitapp.fitness.FitnessService;
+import edu.ucsd.cse110.googlefitapp.fitness.FitnessServiceFactory;
+import edu.ucsd.cse110.googlefitapp.fitness.GoogleFitnessServiceFactory;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -13,19 +27,104 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 public class NewGoalDialogUnitTest {
-    @Test
-    public void testNullNewInstance() {
-        NewGoalDialog newGoalDialog = NewGoalDialog.newInstance(null, 0);
-        assertNull(newGoalDialog);
+    private static final String TEST_SERVICE = "TEST_NEW_GOAL_DIALOG";
+    private DialogFragment dialogFragment;
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        FitnessServiceFactory googleFitnessServiceFactory = new GoogleFitnessServiceFactory();
+
+        googleFitnessServiceFactory.put(TEST_SERVICE, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(Activity mainActivity) {
+                return new TestFitnessService(mainActivity);
+            }
+        });
+
+        Intent intent = new Intent(RuntimeEnvironment.application, MainActivity.class);
+        intent.putExtra("TEST", true);
+        intent.putExtra("TEST_SERVICE_MAIN", TEST_SERVICE);
+        intent.putExtra("TEST_SERVICE_STEP_COUNT", TEST_SERVICE);
+        activity = Robolectric.buildActivity(MainActivity.class, intent).create().get();
+        FragmentManager fm = activity.getSupportFragmentManager();
+        dialogFragment = NewGoalDialog.newInstance(activity.getString(R.string.congratsPrompt), 0);
+        dialogFragment.show(fm, "fragment_set_new_goal");
     }
 
     @Test
-    public void testNotNullNewInstance() {
-        NewGoalDialog newGoalDialog = NewGoalDialog.newInstance("Test", 5000);
-        assertNotNull(newGoalDialog);
-        assertNotNull(newGoalDialog.getArguments());
-        assertEquals("Test", newGoalDialog.getArguments().getString("title"));
-        assertEquals(5000, newGoalDialog.getCurrentGoal());
-        assertTrue(newGoalDialog.isCancelable());
+    public void testFirstHeightIsShown() {
+        assertNotNull(dialogFragment);
+        assertTrue(dialogFragment.getShowsDialog());
+    }
+
+    @Test
+    public void testDialogCancelable() {
+        assertTrue(dialogFragment.isCancelable());
+    }
+
+    @Test
+    public void testHeightDialogBindToMain() {
+        assertNotNull(dialogFragment.getActivity());
+        assertEquals(activity, dialogFragment.getContext());
+    }
+
+    @Test
+    public void testHeightDialogTag() {
+        assertEquals("fragment_set_new_goal", dialogFragment.getTag());
+    }
+
+    private class TestFitnessService implements FitnessService {
+        private static final String TAG = "[TestStepCountActFitnessService]: ";
+        private Activity mainActivity;
+
+        public TestFitnessService(Activity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public int getRequestCode() {
+            return 0;
+        }
+
+        @Override
+        public void setup() {
+            Log.d(TAG, "setup");
+        }
+
+        @Override
+        public void updateStepCount() {}
+
+        @Override
+        public void stopAsync() {
+        }
+
+        @Override
+        public void startAsync() {
+        }
+
+        @Override
+        public boolean hasPermission() {
+            return false;
+        }
+
+        @Override
+        public void addInactiveSteps(int extraStep) {
+        }
+
+        @Override
+        public void addActiveSteps(int step, int min, int sec, float stride) {
+
+        }
+
+        @Override
+        public String getUID() {
+            return null;
+        }
+
+        @Override
+        public String getEmail() {
+            return null;
+        }
     }
 }
