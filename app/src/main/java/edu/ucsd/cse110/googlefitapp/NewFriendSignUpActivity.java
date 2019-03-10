058f0413c468c1749f0770cf8c98283670074c5e
EnqiZhang99 <enz004@ucsd.com>
2019-03-09 20:44:14 -0800

    add request sent menu and finish US 5 #108

--- app/src/main/java/edu/ucsd/cse110/googlefitapp/NewFriendSignUpActivity.java
+++ app/src/main/java/edu/ucsd/cse110/googlefitapp/NewFriendSignUpActivity.java
@@ -11,11 +11,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -72,12 +75,12 @@ public class NewFriendSignUpActivity extends AppCompatActivity {
                             boolean userExist = false;
                             String friendUid = null;
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.e(TAG, document.getId() + " => " + document.getData());
-                                Log.e(TAG, " => ");
-                                Log.e(TAG, "Requested friend's email => " + friendEmail);
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Log.d(TAG, " => ");
+                                Log.d(TAG, "Requested friend's email => " + friendEmail);
 
                                 if(document.getData().get("email").equals(friendEmail) ){
-                                    Log.e(TAG, "User exists!");
+                                    Log.d(TAG, "User exists!");
                                     userExist = true;
                                     friendUid = (String) document.getData().get("id");
                                 }
@@ -88,22 +91,28 @@ public class NewFriendSignUpActivity extends AppCompatActivity {
                                 return;
                             }
 
-                            DocumentReference friendship = FirebaseFirestore.getInstance()
-                                    .collection("friendship")
-//                                    .document("user")
-                                    .document(NewFriendSignUpActivity.this.getIntent().getStringExtra("uid"));
-//                                    .collection(NewFriendSignUpActivity.this.getIntent().getStringExtra("uid"));
+                            String uid = NewFriendSignUpActivity.this.getIntent().getStringExtra("uid");
+
                             Map<String, Object> friend = new HashMap<>();
                             friend.put(friendUid, true);
                             friend.put("email", NewFriendSignUpActivity.this.getIntent().getStringExtra("email"));
 //                            friend.put("email", friendEmail);
-                            friendship.set(friend).addOnSuccessListener(result -> {
-                                Log.e(TAG, "Friend request sent successfully");
-                                friendEmailTxt.setText("");
-                                showToast("Request Send");
-                            }).addOnFailureListener(error -> {
-                                Log.e(TAG, error.getLocalizedMessage());
-                            });
+
+                            FirebaseFirestore.getInstance().collection("friendship").document(uid).set(friend, SetOptions.merge())
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d(TAG, "Friend request sent successfully");
+                                            friendEmailTxt.setText("");
+                                            showToast("Request Send");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.e(TAG, e.getLocalizedMessage());
+                                        }
+                                    });
                         } else {
                             Log.e(TAG, "Error getting documents: ", task.getException());
                         }
